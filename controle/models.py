d5b587ba09bcb7707291968dca4b34e24ed6296a
LucasMonteiroBastos <monteiro.bastos1994@gmail.com>
2020-12-11 10:26:21 -0300

    criado os models com os respectivos migrates

--- controle/models.py
+++ controle/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+class Matricula(models.Model):
+    matricula = models.CharField(max_length=11)
+
+    def __str__(self):
+        return self.matricula
+
+class Turma(models.Model):
+    turma = models.CharField(max_length=3)
+
+    def __str__(self):
+        return self.turma
+
+
+class Turno(models.Model):
+    turno = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.turno
+
+
+
+
+class Aluno(models.Model):
+    matricla = models.ForeignKey(Matricula, on_delete=models.PROTECT)
+    turma = models.ForeignKey(Turma,on_delete=models.PROTECT)
+    turno = models.ForeignKey(Turno, max_length=20, on_delete=models.PROTECT)
+    nome = models.CharField(max_length=100)
+    disciplina = models.CharField(max_length=20)
+
+
+    def __str__(self):
+        return self.nome
+
+
--- controle/views.py
+++ controle/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'controle.html')
+    nome = 'Alunos'
+    return render(request, 'controle.html', {'nome': nome})
